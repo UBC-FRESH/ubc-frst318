76fd978d966c079cd7dee650d0c5bdaa25be538a
Gregory Paradis <greg@globaloptimality.com>
2018-10-23 20:23:54 +0000

    working demo

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 import os
-ws3_path = './lib/ws3/ws3'
+ws3_path = './lib/ws3'
 os.sys.path.insert(0, ws3_path)
+import ws3
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
